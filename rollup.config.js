2dadc1d55e6e6dabfaa832176008024283e1afaf
Bogdan Chadkin <trysound@yandex.ru>
2019-06-04 02:57:07 +0300

    Upgrade build (#18)

--- rollup.config.js
+++ rollup.config.js
@@ -1,59 +1,42 @@
 // @flow
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
 
 const input = 'src/index.js';
+const name = 'rafSchd';
 
 export default [
   // Universal module definition (UMD) build
   {
     input,
-    output: {
-      file: 'dist/raf-schd.js',
-      format: 'umd',
-      name: 'rafSchd',
-    },
+    output: { format: 'umd', file: 'dist/raf-schd.js', name },
     plugins: [
-      // Setting development env before running babel etc
-      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
       babel(),
-      commonjs({ include: 'node_modules/**' }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
     ],
   },
   // Universal module definition (UMD) build (production)
   {
     input,
-    output: {
-      file: 'dist/raf-schd.min.js',
-      format: 'umd',
-      name: 'rafSchd',
-    },
+    output: { format: 'umd', file: 'dist/raf-schd.min.js', name },
     plugins: [
-      // Setting production env before running babel etc
-      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
       babel(),
-      commonjs({ include: 'node_modules/**' }),
-      uglify(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      terser(),
     ],
   },
   // ESM build
   {
     input,
-    output: {
-      file: 'dist/raf-schd.esm.js',
-      format: 'es',
-    },
+    output: { format: 'esm', file: pkg.module },
     plugins: [babel()],
   },
   // CommonJS build
   {
     input,
-    output: {
-      file: 'dist/raf-schd.cjs.js',
-      format: 'cjs',
-    },
+    output: { format: 'cjs', file: pkg.main },
     plugins: [babel()],
   },
 ];
